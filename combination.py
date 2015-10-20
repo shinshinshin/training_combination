e48d6fe7ce2badea93935357e6dde3955956d99c
shinshinshin <saekishinya@yahoo.co.jp>
2015-10-21 07:29:09 +0900

    order

--- combination.py
+++ combination.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-def all_combination(n,k):
+def all_ordered_combination(n,k):
 	if k == 1:
 		li = map(lambda m: [m],range(n))
 		return li;
 	else:
-		li = all_combination(n,k-1)
+		li = all_ordered_combination(n,k-1)
 		new_li = list()
 		for t_l in li:
 			for t_k in range(n):
@@ -15,5 +15,16 @@ def all_combination(n,k):
 					tmp_l.append(t_k)
 					new_li.append(tmp_l)
 		return new_li
+
+def all_combination(n,k):
+	if k == 0:
+		return [[]]
+	elif n == k:
+		return [range(0,n)]
+	else:
+		li = all_combination(n-1,k)
+		li2 = all_combination(n-1,k-1)
+		li2 = map(lambda e: e+[n-1] ,li2)
+		return li+li2
 		
-print all_combination(4,3)
+print all_combination(4,1)
